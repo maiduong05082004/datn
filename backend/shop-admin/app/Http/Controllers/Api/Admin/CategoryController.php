1199f009b551185739c2f6cefe85a5e0ecf11e48
maiduong <duongmdph40323@gmail.com>
2024-10-18 06:01:03 +0700

    Fix Update Image Api Category

--- backend/shop-admin/app/Http/Controllers/Api/Admin/CategoryController.php
+++ backend/shop-admin/app/Http/Controllers/Api/Admin/CategoryController.php
@@ -63,6 +63,8 @@ class CategoryController extends Controller
                 Storage::disk('public')->delete($category->image);
             }
             $data['image'] = $request->file('image')->store('categories', 'public');
+        } else{
+            return response()->json(['error' => 'Không có ảnh được tải lên.'], 400);
         }
     
 
