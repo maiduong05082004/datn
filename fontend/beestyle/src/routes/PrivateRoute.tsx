2fc01a3f1c37ce88f7068a0234387ffc16cf8161
chikim04 <nguyenkimchi26042004@gmail.com>
2024-11-22 23:38:54 +0700

    Check Role Admin

--- fontend/beestyle/src/routes/PrivateRoute.tsx
+++ fontend/beestyle/src/routes/PrivateRoute.tsx
@@ -3,16 +3,17 @@ import React from 'react'
 import { Navigate } from 'react-router-dom'
 
 type Props = {
-    children: React.ReactNode
+    children: React.ReactNode;
+    userRole: string | null;
 }
 
-const PrivateRouter = ({ children }: Props) => {
+const PrivateRouter = ({ children, userRole }: Props) => {
     const token = localStorage.getItem('token');
     const user = JSON.parse(localStorage.getItem('user') || '{}');
     console.log(token, user);
 
     let check: boolean = true;
-    if (!token || user?.role !== 'admin') { 
+    if (!token || user?.role !== 'admin') {
         check = false;
         message.error("Bạn không có quyền truy cập");
     }
